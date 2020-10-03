e9e192eb021c69f0edf8d88350b49adc00e435f3
Emily Groenendal <emilylauder@gmail.com>
2020-10-02 23:20:31 -0400

    'created questions list and adding variables'

---
+++ app.js
@@ -0,0 +1,11 @@
+// create variable for start time 
+let startTime = 76;
+
+// timer element that displays the time 
+let timer = document.querySelector("#timer");
+
+// start button 
+let startButton = document.querySelector("#start-btn");
+
+// high score  button 
+let highScoreButton = document.querySelector("#start-highScore-btn");
--- questionList.js
+++ questionList.js
@@ -1,34 +1,28 @@
 // list array of questions 
 
 let questions = [{
-        prompt: "",
-        choiceOptions: "",
-        correctAnswer: ""
+        prompt: "What does the <p> tag stand for?",
+        choiceOptions: ["parent", "paragraph", "parakeet", "private"],
+        correctAnswer: "private"
     },
     {
-        prompt: "",
-        choiceOptions: "",
-        correctAnswer: ""
+        prompt: "in CSS, how would you select the class, 'blue' ?",
+        choiceOptions: ["#blue", "blue", ".blue", "blue."],
+        correctAnswer: ".blue"
     },
     {
-        prompt: "",
-        choiceOptions: "",
-        correctAnswer: ""
+        prompt: "In javaScript, what do you add at the end of a function to call (or run) it?",
+        choiceOptions: ["[]", "{}", "()", "++"],
+        correctAnswer: "()"
     },
     {
-        prompt: "",
-        choiceOptions: "",
-        correctAnswer: ""
+        prompt: "In HTML, in what tag, do you give your site a Title?",
+        choiceOptions: ["<body>", "<nav>", "<meta>", "<title>"],
+        correctAnswer: "<title>"
     },
     {
-        prompt: "",
-        choiceOptions: "",
-        correctAnswer: ""
+        prompt: "How do you specify a font in CSS?",
+        choiceOptions: ["font-weight", "font-family", "font", "font-decoration"],
+        correctAnswer: "font-family"
     },
-
-
-
-
-
-
 ]
