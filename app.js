cb8b9cf3d0e167ab0a24d90e1509f642fd7446e4
Emily Groenendal <emilylauder@gmail.com>
2020-10-04 19:32:05 -0400

    'udpating HTML variables'

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 // create variable for time left 
 let timeLeft = 76;
 // question count starting point
-let questionCount = 0;
+let currentQuestionIndex = 0;
 // score starting point
 let score = 0;
-// list arry of questions 
+// correct answer variable 
+let correct;
+// timer element that displays the time 
+let timer = document.querySelector("#timer");
+// quiz container 
+let quizContainer = document.querySelector(".quiz-container");
+// paragraph for questions
+let questionEl = document.querySelector(".qph");
+
+// multiple choice options 
+let choicesEl = document.querySelector(".choiceph");
+
+// answer variables 
+let answerEl = document.querySelector(".answerph");
+
+// start button 
+let startButton = document.querySelector("#start-btn");
+startButton.addEventListener("click", setTimer);
+
+// high score  button 
+let highScoreButton = document.querySelector("#start-highScore-btn");
+
+
+
+// game over/ YOU LOSE div
+let youLoseDiv = querySelector(".youLose");
+
+// list array of questions 
 let questions = [{
     prompt: "What does the <p> tag stand for?",
     choiceOptions: ["parent", "paragraph", "parakeet", "private"],
@@ -27,24 +54,7 @@ let questions = [{
     correctAnswer: "font-family"
 }, ]
 
-// timer element that displays the time 
-let timer = document.querySelector("#timer");
 
-// start button 
-let startButton = document.querySelector("#start-btn");
-startButton.addEventListener("click", setTimer);
-
-// high score  button 
-let highScoreButton = document.querySelector("#start-highScore-btn");
-
-// paragraph for questions
-let questionDiv = document.querySelector(".qph");
-
-// multiple choice options 
-let choicesDiv = document.querySelector(".choiceph");
-
-// answer variables 
-let answerDiv = document.querySelector(".answerph");
 // function to convert mins to seconds
 
 function minsToSeconds(minutes) {
@@ -56,27 +66,62 @@ function minsToSeconds(minutes) {
 
 // scoring system what is the right answer and what is the ultimate win score/time
 
-// convert 5 mins to seconds - find mathematical way to convert seconds to mins & vice versa
-
 // object -- key pairs object questions answers 
 
 // when the start button is clicked, start the timer and show question one 
 
-function setTimer() {
+function generateQuestions() {
+    youLoseDiv.style.display = "none";
+
+    if (currentQuestionIndex === finalQuestionIndex) {
+        return showScore();
+    }
+    let currentQuestion = questions[currentQuestionIndex];
 
 
-    let timeInterval = setInterval(function () {
-        timeLeft--;
-        timer.textContent = "";
-        timer.textContent = "Time" + timeLeft;
-
-        if (timeLeft <= 0 || questionCount === questions.length) {
-            clearInterval(timeInterval);
-
-        }
-    }, 1000);
 }
 
 function startQuiz() {
+    let displayQuestions;
+    // show questions 
+
+    // if user gets answer right show next question and add score
+    if (user)
+        //if user gets answer wrong, minus time, show next question and add score
+
+
+        let timeInterval = setInterval(function () {
+            timeLeft--;
+            timer.textContent = "Time:" + timeLeft;
+
+            if (timeLeft <= 0 || questionCount === questions.length) {
+                clearInterval(timeInterval);
+                showScore();
+            }
+        }, 1000);
+};
+
+
+
+// show score function
+
+function showScore() {
+
+}
+// choice variables
+let userChoice;
+let finalQuestionIndex = questions.length;
+
+function checkAnswer(answer) {
+    correct = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (answer === correct && currentQuestionIndex !== finalQuestionIndex) {
+        score++;
+        currentQuestionIndex++
+    } else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex) {
+        currentQuestionIndex++
+    } else {
+        score;
+    }
 
 }
